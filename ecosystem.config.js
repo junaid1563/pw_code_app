d6cb0ef14753c0b663621012584bfbef7dd561d8
junaid1563 <71489463+junaid1563@users.noreply.github.com>
2025-01-05 18:56:24 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         script: "npm", // npm start
         args: "run preprods", // Check package json above
         listen_timeout: 12000, // Hack Without timeout we will get downtime in deployment
-        cwd: "/home/ec2-user", // Command execution path
+        cwd: "/home/ubuntu/pw_code_app", // Command execution path
         instances: "max", // Max Use all the CPU
         exec_mode: "cluster", // Clustor Mode
         increment_var: "PORT", // It will auto increment port your app will run on 3001, 3002 and so on
